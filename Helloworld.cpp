1fef051ddd02eceffc2ef7718d8574e49c3d211f
Amara-Manikanta <amaramanikantadileep@gmail.com>
2024-10-06 22:04:48 +0530

    changes

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main() {
+  cout << "Hello World! \n\n";
+  cout << "I am learning C++";
+  return 0;
 }
